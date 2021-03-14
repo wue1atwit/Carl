0d4cb55d8dee9929c4099f48c4094f1d78aadad3
Ethan WIT <wue1@wit.edu>
2021-03-13 23:06:17 -0500

    Setup

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class Game {
+public class Game extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Pane root = new Pane();
+
+		Scene scene = new Scene(root,600,600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
